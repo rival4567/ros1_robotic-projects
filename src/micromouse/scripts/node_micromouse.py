c0931669b0eb0e26798c7afe994f7c47449c56d3
shivam <kumarshivam4567@gmail.com>
2021-07-06 16:53:41 +0530

    complete solution of micromouse

--- src/micromouse/scripts/node_micromouse.py
+++ src/micromouse/scripts/node_micromouse.py
@@ -90,7 +90,7 @@ def fix_yaw(des_pos):
 
     twist_msg = Twist()
     if math.fabs(err_yaw) > yaw_precision_:
-        twist_msg.angular.z = 0.2 if err_yaw > 0 else -0.2
+        twist_msg.angular.z = 0.3 if err_yaw > 0 else -0.3
 
     pub.publish(twist_msg)
 
@@ -109,7 +109,7 @@ def go_straight_ahead(des_pos):
 
     if err_pos > dist_precision_:
         twist_msg = Twist()
-        twist_msg.linear.x = 0.7
+        twist_msg.linear.x = 1.0
         # twist_msg.angular.z = 0.05 if err_yaw > 0 else -0.05
         pub.publish(twist_msg)
     else:
